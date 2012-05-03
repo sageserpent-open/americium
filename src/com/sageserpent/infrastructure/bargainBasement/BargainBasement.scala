56b80df1a816cd68fae00da4d8bcb69a60b35a78
gjm <g.j.murphy@sageserpent.com>
2012-05-03 15:16:50 +0100

    Ported some combinatoric mathematics over.
    --HG--
    extra : rebase_source : 4daf7dad9cdb92d66902237bb76fa3986aeb0038

--- src/com/sageserpent/infrastructure/bargainBasement/BargainBasement.scala
+++ src/com/sageserpent/infrastructure/bargainBasement/BargainBasement.scala
@@ -5,7 +5,7 @@ object BargainBasement {
     for (Seq(first, second) <- iterable.sliding(2))
       yield first -> second
   }
-  
+
   def isSorted[X <% Ordered[X]](iterable: Iterable[X]) = {
     iterable.sliding(2).forall {
       case Seq(first, second) => first <= second
@@ -26,5 +26,26 @@ object BargainBasement {
       }
     }
   }
-}
 
+  def numberOfPermutations(originalSize: Int, permutationSize: Int) = {
+    require(originalSize >= 0)
+    require(permutationSize >= 0)
+
+    if (permutationSize > originalSize)
+      0
+    else {
+      val numberOfItemsLeftOutOfPermutation = originalSize - permutationSize
+      def productOfPartialResultAndNumberOfSubpermutations(originalSize: Int, partialResult: Int): Int =
+        if (originalSize == numberOfItemsLeftOutOfPermutation)
+          partialResult
+        else productOfPartialResultAndNumberOfSubpermutations((originalSize - 1), (originalSize * partialResult))
+      productOfPartialResultAndNumberOfSubpermutations(originalSize, 1)
+    }
+  }
+
+  def factorial(x: Int) =
+    numberOfPermutations(x, x)
+
+  def numberOfCombinations(originalSize: Int, combinationSize: Int) =
+    numberOfPermutations(originalSize, combinationSize) / factorial(combinationSize)
+}
