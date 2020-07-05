66debe40cd3a83ad68310fce0925822861d078cc
Gerard Murphy <gjmurphy1@icloud.com>
2020-07-05 10:17:59 +0100

    Extend spike to automatically derive a Hedgehog 'Gen[T]' instance via Magnolia - now supports algebraic data types, aka sum and product types.

--- src/test/scala/com/sageserpent/americium/magnoliaGen.scala
+++ src/test/scala/com/sageserpent/americium/magnoliaGen.scala
@@ -1,6 +1,6 @@
 package com.sageserpent.americium
 
-import hedgehog.core.{DiscardCount, PropertyConfig, Seed, ShrinkLimit, SuccessCount}
+import hedgehog.core._
 import hedgehog.{Gen, Property, Range, Result}
 import magnolia._
 import mercator._
@@ -22,13 +22,21 @@ object magnoliaGen {
     override def map[A, B](from: Typeclass[A])(fn: A => B): Typeclass[B] =
       from.map(fn)
   }
-  def combine[T](caseClass: CaseClass[Typeclass, T]): Typeclass[T] = {
-    // This is where I decided that the nominally square peg would indeed
-    // fit snugly into the round hole ... and lo, it did!
+
+  def combine[T](caseClass: CaseClass[Typeclass, T]): Typeclass[T] =
+    // TODO - use a wildcard capture or something to work in hand with
+    // the Scala type system.
     caseClass.constructMonadic[Gen, Any](parameter =>
       parameter.typeclass.asInstanceOf[Gen[Any]])
+
+  def dispatch[T](sealedTrait: SealedTrait[Typeclass, T]): Typeclass[T] = {
+    // TODO - use a wildcard capture or something to work in hand with
+    // the Scala type system.
+    val subtypeGenerators: Seq[Typeclass[T]] =
+      sealedTrait.subtypes.map(_.typeclass).asInstanceOf[Seq[Typeclass[T]]]
+    Gen.choice(subtypeGenerators.head, subtypeGenerators.tail.toList)
   }
-  def dispatch[T](sealedTrait: SealedTrait[Typeclass, T]): Typeclass[T] = ???
+
   implicit def gen[T]: Typeclass[T] = macro Magnolia.gen[T]
 }
 
@@ -36,15 +44,15 @@ object testBed extends App {
   implicit val ints: Gen[Int]       = Gen.int(Range.linear(1, 45))
   implicit val strings: Gen[String] = Gen.string(Gen.alpha, Range.linear(1, 35))
 
-  val tuples: Gen[Tuple2[Int, String]] = magnoliaGen.gen
+  val eithers: Gen[Either[Int, String]] = magnoliaGen.gen
 
   val configuration: PropertyConfig =
     PropertyConfig(SuccessCount(20000), DiscardCount(200), ShrinkLimit(20000))
 
   val report = Property.check(
     configuration,
-    tuples.forAll
-      .map { case (int, string) => println(int, string) }
+    eithers.forAll
+      .map(println)
       .map(_ => Result.success),
     Seed.fromLong(1L)
   )
