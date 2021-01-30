74705a37b06d857da0de453580eac55c44bb267d
Gerard Murphy <gjmurphy1@icloud.com>
2021-01-30 15:43:52 +0000

    Add test for exceptional cases amongst a choice. Test passes.

--- src/test/scala/com/sageserpent/americium/TrialsSpec.scala
+++ src/test/scala/com/sageserpent/americium/TrialsSpec.scala
@@ -11,11 +11,6 @@ class TrialsSpec
     with TableDrivenPropertyChecks {
   autoVerify = false
   /*
-  1.   A constant case yields just one trial.
-  2.   A constant case that provokes an exception will result in an exception referencing that case.
-  3.   A choice yields all and only the cases given to the choice.
-  4.   A choice that includes cases that provoke exceptions will result in an exception that references one of those cases.
-  5.   An alternation yields all and only the cases that would be covered by the alternates in isolation.
   6.   An alternation that over trials that in isolation include cases that provoke exceptions will result in an exception that references one of those cases.
   7.   In general, trials yield the same cases.
   8.   In general, trials result in the same exception and referenced case (and recipe).
@@ -77,6 +72,53 @@ class TrialsSpec
           mockConsumer.verify(possibleChoice))
       }
     }
+
+  case class ExceptionWithCasePayload[Case](caze: Case) extends RuntimeException
+
+  "a choice that includes exceptional cases" should "result in one of the corresponding exceptions" in {
+    type ChoicesAndCriterion[X] = (Seq[X], X => Boolean)
+
+    def testBodyInWildcardCapture[X](
+        choicesAndCriterion: ChoicesAndCriterion[X]) =
+      withExpectations {
+        choicesAndCriterion match {
+          case (possibleChoices, exceptionCriterion) =>
+            val sut = Trials.choose(possibleChoices)
+
+            val mockConsumer = { caze: X =>
+              if (exceptionCriterion(caze))
+                throw ExceptionWithCasePayload(caze)
+            }
+
+            val exception = intercept[sut.TrialException] {
+              sut.supplyTo(mockConsumer)
+            }
+
+            val underlyingException = exception.getCause
+
+            underlyingException shouldBe a[ExceptionWithCasePayload[_]]
+
+            underlyingException match {
+              case exceptionWithCasePayload: ExceptionWithCasePayload[_] =>
+                exception.provokingCase should be(exceptionWithCasePayload.caze)
+
+                exactly(1, possibleChoices) should be(
+                  exceptionWithCasePayload.caze)
+            }
+        }
+      }
+
+    forAll(
+      Table[ChoicesAndCriterion[_]](
+        "possibleChoices -> exceptionCriterion",
+        (1 to 10, 0 == (_: Int) % 2),
+        (-5 to 5 map (_.toString), (_: String).contains("5")),
+        (Seq(false, true), identity[Boolean] _),
+        (Seq(4.3), (_: Double) => true)
+      )) { choicesAndCriterion =>
+      testBodyInWildcardCapture(choicesAndCriterion)
+    }
+  }
 
   "an alternation" should "yield all and only the cases that would be yielded by its alternatives" in
     forAll(
