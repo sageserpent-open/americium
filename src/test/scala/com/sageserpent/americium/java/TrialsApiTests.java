d812727ced0ef9b9ffef1c74f209ba057fbbc32b
Gerard Murphy <gjmurphy1@icloud.com>
2021-02-01 13:13:48 +0000

    Make the Java smoke test of 'TrialsApi' more interesting with an example of 'BigDecimal' versus 'Double'. This exposes a problem with the API - the Java and Scala overloads are fighting each other.

--- src/test/scala/com/sageserpent/americium/java/TrialsApiTests.java
+++ src/test/scala/com/sageserpent/americium/java/TrialsApiTests.java
@@ -3,18 +3,26 @@ package com.sageserpent.americium.java;
 import com.sageserpent.americium.Trials;
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
+import java.util.function.Function;
+
 public class TrialsApiTests {
     @Test
     void testDrivePureJavaApi() {
         final TrialsApi api = Trials.api();
 
         final Trials<Integer> integerTrials = api.only(1);
-        final Trials<Double> doubleTrials = api.choose(new Double[]{1.2, 5.6});
+        final Trials<Double> doubleTrials =
+                api.choose(new Double[]{1.2, 5.6, 0.1 + 0.1 + 0.1})
+                        .map((Function<? super Double, Double>) value -> 10 * value);
+        final Trials<BigDecimal> bigDecimalTrials =
+                api.only(BigDecimal.ONE.divide(BigDecimal.TEN).add(BigDecimal.ONE.divide(BigDecimal.TEN)).add(BigDecimal.ONE.divide(BigDecimal.TEN)))
+                        .map((Function<? super BigDecimal, BigDecimal>) value -> value.multiply(BigDecimal.TEN));
 
-        final Trials<? extends Number> alternateTrials = api.alternate(integerTrials, doubleTrials, doubleTrials);
+        final Trials<? extends Number> alternateTrials = api.alternate(integerTrials, doubleTrials, bigDecimalTrials);
 
         alternateTrials.supplyTo(number -> {
-            System.out.println(number.floatValue());
+            System.out.println(number);
         });
     }
 }
