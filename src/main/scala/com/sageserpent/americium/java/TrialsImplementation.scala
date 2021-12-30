32c4ab35941b39b6ea700423a309de24bf3531f5
Gerard Murphy <gjmurphy1@icloud.com>
2021-12-30 11:33:16 +0000

    Reformatting.

--- src/main/scala/com/sageserpent/americium/java/TrialsImplementation.scala
+++ src/main/scala/com/sageserpent/americium/java/TrialsImplementation.scala
@@ -525,9 +525,8 @@ case class TrialsImplementation[Case](
               // factor - if it's too high, say 2, then the solutions are hardly
               // shrunk at all. If it is unity, then the solutions are shrunk a
               // bit but can be still involve overly 'large' values, in the
-              // sense
-              // that the factory input values are large. This needs finessing,
-              // but will do for now...
+              // sense that the factory input values are large. This needs
+              // finessing, but will do for now...
               val limitWithExtraLeewayThatHasBeenObservedToFindBetterShrunkCases =
                 (100 * limit / 99) max limit
 
