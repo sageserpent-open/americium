adb4b0d4b64735178b8c3dfa7daec40b60e2ff8b
Gerard Murphy <gjmurphy1@icloud.com>
2023-02-20 20:11:19 +0000

    Disable example test that is supposed to fail so that the build passes overall.

--- src/test/scala/com/sageserpent/americium/java/GroupingTest.java
+++ src/test/scala/com/sageserpent/americium/java/GroupingTest.java
@@ -1,12 +1,18 @@
 package com.sageserpent.americium.java;
 
 import com.google.common.collect.ImmutableList;
+import org.junit.jupiter.api.Disabled;
 
 import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
+/**
+ * This is expected to fail, it is used as an example of finding a failure
+ * and shrinking the failing test case.
+ */
+@Disabled
 class GroupingTest {
     private static final TrialsScaffolding.SupplyToSyntax<ImmutableList<Integer>>
             testConfiguration = Trials
