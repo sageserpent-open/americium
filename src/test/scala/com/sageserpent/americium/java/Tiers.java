5533b42ddf5a8dd285a3c69c2f5c8c8b9d5e9043
Gerard Murphy <gjmurphy1@icloud.com>
2023-02-25 12:39:00 +0000

    Add `TiersTest` to support documentation.

---
+++ src/test/scala/com/sageserpent/americium/java/Tiers.java
@@ -0,0 +1,51 @@
+package com.sageserpent.americium.java;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+class Tiers<Element extends Comparable<Element>> {
+    final int worstTier;
+
+    final List<Element> storage;
+
+    public Tiers(int worstTier) {
+        this.worstTier = worstTier;
+        storage = new ArrayList<>(worstTier) {
+            @Override
+            public void add(int index, Element element) {
+                if (size() < worstTier) {
+                    super.add(index, element);
+                } else if (0 < index) {
+                    for (int shiftDestination = 0;
+                         shiftDestination < index - 1; ++shiftDestination) {
+                        super.set(shiftDestination,
+                                  super.get(1 + shiftDestination));
+                    }
+
+                    super.set(index - 1, element);
+                }
+            }
+        };
+    }
+
+    void add(Element element) {
+        final int index = Collections.binarySearch(storage, element);
+
+        // (0 > index) Not detected - why?
+        if (0 >= index) {
+            if (0 == index) System.out.format("Ouch: %s\n", element);
+            storage.add(-(index + 1), element);
+        } else {
+            storage.add(index, element);
+        }
+    }
+
+    Optional<Element> at(int tier) {
+        return 0 < tier && tier <= storage.size()
+               ? Optional.of(storage.get(storage.size() - tier))
+               :
+               Optional.empty();
+    }
+}
---
+++ src/test/scala/com/sageserpent/americium/java/TiersTest.java
@@ -0,0 +1,102 @@
+package com.sageserpent.americium.java;
+
+import com.google.common.collect.ImmutableList;
+import cyclops.data.tuple.Tuple;
+import cyclops.data.tuple.Tuple2;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static com.sageserpent.americium.java.Trials.api;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+
+public class TiersTest {
+    private final static Trials<ImmutableList<Integer>> queryValueLists = api()
+            .integers(-1000, 1000)
+            .immutableLists()
+            .filter(list -> !list.isEmpty());
+
+
+    private final static Trials<Tuple2<ImmutableList<Integer>,
+            ImmutableList<Integer>>>
+            testCases =
+            queryValueLists.flatMap(queryValues -> {
+                final int minimumQueryValue =
+                        queryValues.stream().min(Integer::compareTo).get();
+
+                // A background is a (possibly empty) run of values that are
+                // all less than the query values.
+                final Trials<ImmutableList<Integer>> backgrounds = api()
+                        .integers(Integer.MIN_VALUE, minimumQueryValue - 1)
+                        .immutableLists();
+
+                // A section is either a query value in a singleton list, or
+                // a background.
+                final List<Trials<ImmutableList<Integer>>> sectionTrials =
+                        queryValues
+                                .stream()
+                                .flatMap(queryValue ->
+                                                 Stream.of(api().only(
+                                                                   ImmutableList.of(
+                                                                           queryValue)),
+                                                           backgrounds))
+                                .collect(Collectors.toList());
+
+                sectionTrials.add(0, backgrounds);
+
+                // Glue the trials together and flatten the sections they
+                // yield into a single feed sequence per trial.
+                final Trials<ImmutableList<Integer>> feedSequences =
+                        api().immutableLists(sectionTrials).map(sections -> {
+                            final ImmutableList.Builder<Integer> builder =
+                                    ImmutableList.builder();
+                            sections.forEach(builder::addAll);
+                            return builder.build();
+                        });
+                return feedSequences.map(feedSequence -> Tuple.tuple(queryValues,
+                                                                     feedSequence));
+            });
+
+    @TrialsTest(trials = "testCases", casesLimit = 10)
+    void tiersShouldRetainTheLargestElements(Tuple2<ImmutableList<Integer>,
+            ImmutableList<Integer>> testCase) {
+        final ImmutableList<Integer> queryValues = testCase._1();
+        final ImmutableList<Integer> feedSequence = testCase._2();
+
+        System.out.format("Query values: %s, feed sequence: %s\n",
+                          queryValues,
+                          feedSequence);
+
+        final int worstTier = queryValues.size();
+
+        final Tiers<Integer> tiers = new Tiers<>(worstTier);
+
+        feedSequence.forEach(tiers::add);
+
+        final ImmutableList.Builder<Integer> builder =
+                ImmutableList.builder();
+
+        int tier = worstTier;
+
+        int previousTierOccupant = Integer.MIN_VALUE;
+
+        do {
+            final Integer tierOccupant = tiers.at(tier).get();
+
+            assertThat(tierOccupant,
+                       greaterThanOrEqualTo(previousTierOccupant));
+
+            builder.add(tierOccupant);
+
+            previousTierOccupant = tierOccupant;
+        } while (1 < tier--);
+
+        final ImmutableList<Integer> arrangedByRank = builder.build();
+
+        assertThat(arrangedByRank,
+                   containsInAnyOrder(queryValues.toArray()));
+    }
+}
