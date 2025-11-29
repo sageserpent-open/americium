610eb5f098a3566c2e6db3894b643481178fff24
Gerard Murphy <gjmurphy1@icloud.com>
2025-11-29 14:13:49 +0000

    Cut over the Scala form of `PropertyTest` to being a fully automated bug reproduction test; this doesn't require fiddling around with running the entire suite versus individual tests. The expectations now are more relevant to the crux of the bug. Test still fails, as expected...

---
+++ src/test/scala/com/sageserpent/americium/bugReports/Issue255BugReproduction.scala
@@ -0,0 +1,84 @@
+package com.sageserpent.americium.bugReports
+
+import com.eed3si9n.expecty.Expecty.assert
+import com.sageserpent.americium.Trials
+import com.sageserpent.americium.Trials.api as trialsApi
+import com.sageserpent.americium.bugReports.Issue255BugReproduction.HiddenTestSuite
+import com.sageserpent.americium.junit5.*
+import org.junit.jupiter.api.{Disabled, Test, TestFactory}
+import org.junit.jupiter.engine.descriptor.TestFactoryTestDescriptor
+import org.junit.platform.engine.TestExecutionResult
+import org.junit.platform.engine.discovery.DiscoverySelectors
+import org.junit.platform.testkit.engine.{
+  EngineExecutionResults,
+  EngineTestKit,
+  EventType
+}
+
+import scala.jdk.OptionConverters.*
+import scala.jdk.StreamConverters.*
+
+object Issue255BugReproduction {
+
+  private val sharedSyntaxSupply: Trials[Int]#SupplySyntaxType =
+    trialsApi.integers(1, 100).withLimit(50)
+
+  @Disabled
+  class HiddenTestSuite {
+    @TestFactory
+    def firstTestUsingTheSharedSyntaxSupply(): DynamicTests =
+      sharedSyntaxSupply.dynamicTests { value =>
+        println(s"firstTestUsingTheSharedSyntaxSupply: $value")
+      }
+
+    @TestFactory
+    def secondTestUsingTheSharedSyntaxSupply(): DynamicTests =
+      sharedSyntaxSupply.dynamicTests { value =>
+        println(s"secondTestUsingTheSharedSyntaxSupply: $value")
+      }
+  }
+}
+
+class Issue255BugReproduction {
+  @Test
+  def allTestsUsingASharedSyntaxSupplyShouldRun(): Unit = {
+    val results: EngineExecutionResults = EngineTestKit
+      .engine("junit-jupiter")
+      .selectors(DiscoverySelectors.selectClass(classOf[HiddenTestSuite]))
+      .configurationParameter(
+        "junit.jupiter.conditions.deactivate",
+        "org.junit.*DisabledCondition"
+      )
+      .execute
+
+    val overallTestOutcomes = results
+      .containerEvents()
+      .stream()
+      .toScala(List)
+      .filter(event =>
+        EventType.FINISHED == event.getType && event.getTestDescriptor
+          .isInstanceOf[TestFactoryTestDescriptor]
+      )
+      .flatMap(_.getPayload().toScala)
+      .collect { case testExecutionResult: TestExecutionResult =>
+        testExecutionResult.getStatus
+      }
+
+    assert(
+      overallTestOutcomes.forall(TestExecutionResult.Status.SUCCESSFUL == _)
+    )
+
+    val fineGrainedTestDisplayNames = results
+      .testEvents()
+      .stream()
+      .toScala(List)
+      .filter(EventType.FINISHED == _.getType)
+      .map(_.getTestDescriptor)
+      .map(_.getDisplayName)
+
+    assert(
+      2 * fineGrainedTestDisplayNames.toSet.size == fineGrainedTestDisplayNames.size,
+      "Each test case from the shared supply syntax should be run twice, once in the first hidden test, then again in the second hidden test."
+    )
+  }
+}
