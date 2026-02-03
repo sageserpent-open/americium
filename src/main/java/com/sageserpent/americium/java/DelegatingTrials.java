f2316a9bb2f7d9d2629bb96d0b5372065f677999
Gerard Murphy <gjmurphy1@icloud.com>
2026-02-03 14:43:12 +0000

    Fix bug spotted by manual inspection of the output from "test driving the Java API should not produce smoke" n `TrialsSpec`. The yielded implementation always delegates to the instance *currently* passed as an argument to `DelegatingTrials.delegateTo`, previously the instances passed to the first call setting up the cache entry received all the traffic. Code needs cleaning up, there is use of a raw type...

--- src/main/java/com/sageserpent/americium/java/DelegatingTrials.java
+++ src/main/java/com/sageserpent/americium/java/DelegatingTrials.java
@@ -3,6 +3,7 @@ package com.sageserpent.americium.java;
 import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
 import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.implementation.FieldAccessor;
 import net.bytebuddy.implementation.MethodCall;
 import net.bytebuddy.matcher.ElementMatchers;
 
@@ -10,6 +11,10 @@ public class DelegatingTrials {
     private static final Cache<Class<? extends Trials<?>>, Class<?
             extends Trials<?>>>
             cache = Caffeine.newBuilder().build();
+
+    public interface StateForDelegation<Case> {
+        void delegateTo(Trials underlying);
+    }
 
     public static <Case, SpecialisedTrials extends Trials<Case>> SpecialisedTrials delegateTo(
             Class<? extends SpecialisedTrials> specialisedTrialsClass,
@@ -22,14 +27,19 @@ public class DelegatingTrials {
                     .subclass(specialisedTrialsClass)
                     .name(String.format("DelegatingImplementationFor%1s",
                                         specialisedTrialsClass.getSimpleName()))
+                    .defineField("delegatedTo",
+                                 specialisedTrialsClass.getInterfaces()[0])
+                    .implement(StateForDelegation.class)
                     .method(ElementMatchers
                                     .isPublic()
                                     .and(ElementMatchers.not(ElementMatchers.isDeclaredBy(
                                             specialisedTrialsClass))))
                     .intercept(MethodCall
                                        .invokeSelf()
-                                       .on(underlying)
-                                       .withAllArguments());
+                                       .onField("delegatedTo")
+                                       .withAllArguments())
+                    .method(ElementMatchers.named("delegateTo")).intercept(
+                            FieldAccessor.ofField("delegatedTo"));
 
             return byteBuddy
                     .make()
@@ -37,6 +47,14 @@ public class DelegatingTrials {
                     .getLoaded();
         });
 
-        return (SpecialisedTrials) clazz.newInstance();
+        @SuppressWarnings("unchecked") final var stateForDelegation =
+                (StateForDelegation<Case>) clazz.newInstance();
+
+        stateForDelegation.delegateTo(underlying);
+
+        @SuppressWarnings("unchecked") final var trials =
+                (SpecialisedTrials) stateForDelegation;
+
+        return trials;
     }
 }
