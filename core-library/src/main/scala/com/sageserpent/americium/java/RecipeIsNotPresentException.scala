ae340e2b40f165d97b9f687b7e366cb96165c204
Gerard Murphy <gjmurphy1@icloud.com>
2026-02-26 14:01:57 +0000

    Cut over the diagnostic message for `RecipeIsNotPresentException`.

--- core-library/src/main/scala/com/sageserpent/americium/java/RecipeIsNotPresentException.scala
+++ core-library/src/main/scala/com/sageserpent/americium/java/RecipeIsNotPresentException.scala
@@ -2,9 +2,9 @@ package com.sageserpent.americium.java
 
 import java.nio.file.Path
 
-class RecipeIsNotPresentException(recipeHash: String, databasePath: Path)
+class RecipeIsNotPresentException(recipeHash: String, recipeDirectory: Path)
     extends RuntimeException(
       s"""No recipe found for recipe hash: $recipeHash.
-          |Has the directory containing the RocksDB instance for recipes at $databasePath been deleted?
+          |Has the directory $recipeDirectory containing the recipes been deleted, changed to a different location or been regenerated?
           |Either regenerate the recipe by running the test without Java property `trials.recipeHash`, or use `trials.recipe` with the full recipe JSON.""".stripMargin
     ) {}
